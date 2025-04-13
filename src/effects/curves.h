bfc98f791fc0c9db975024be19707102f151756b
Retr0 <dedsecretr0680@gmail.com>
2025-04-13 18:07:50 +0530

    Update curves.h

--- src/effects/curves.h
+++ src/effects/curves.h
@@ -1,16 +1,15 @@
 #ifndef CURVES_H
 #define CURVES_H
 
-class Curves {
-public:
-    Curves();
-    ~Curves();
+#include <AE_Effect.h>
+#include "../plugin.h"
 
-    void applyCurveAdjustment(float* input, float* output, int length);
-    void setCurvePoints(const std::vector<float>& points);
-    
+class CurvesEffect {
+public:
+    static PF_Err interpolateCurve(const CurveDef& curve, PF_FpLong input, PF_FpLong& output);
+    static PF_Err applyCurves(CrazyCurvesParams* params, PF_Pixel8* input, PF_Pixel8* output);
 private:
-    std::vector<float> curvePoints;
+    static PF_FpLong bezierInterpolate(PF_FpLong p0, PF_FpLong p1, PF_FpLong p2, PF_FpLong p3, PF_FpLong t);
 };
 
 #endif // CURVES_H
