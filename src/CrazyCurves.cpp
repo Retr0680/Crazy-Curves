a31421e6d12bf0697a9ef678af1a867e269e0d24
Retr0 <dedsecretr0680@gmail.com>
2025-04-13 18:19:12 +0530

    Update CrazyCurves.cpp
    Updated to use the new classes

--- src/CrazyCurves.cpp
+++ src/CrazyCurves.cpp
@@ -1,63 +1,15 @@
-#include "AEConfig.h"
-#include "AE_Effect.h"
-#include "AE_EffectCB.h"
-#include "AE_Macros.h"
-#include "Param_Utils.h"
-#include "String_Utils.h"
-#include "AE_Effect.h"
-#include "AE_EffectCB.h"
-#include "AE_EffectUI.h"
-#include "AEGP_SuiteHandler.h"
+// Add these includes at the top
+#include "CurvesData.h"
+#include "CurvesUI.h"
 
-#define	MAJOR_VERSION 1
-#define	MINOR_VERSION 0
-#define	BUG_VERSION	0
-#define	STAGE_VERSION 0
-#define	BUILD_VERSION 1
+// Add these as global variables
+static CurvesData g_rgbCurve;
+static CurvesData g_redCurve;
+static CurvesData g_greenCurve;
+static CurvesData g_blueCurve;
+static CurvesUI g_curvesUI;
 
-// Parameter IDs
-enum {
-    PARAM_INPUT = 0,
-    PARAM_RED_CURVE,
-    PARAM_GREEN_CURVE,
-    PARAM_BLUE_CURVE,
-    PARAM_RGB_CURVE,
-    NUM_PARAMS
-};
-
-// Parameter defaults
-static PF_Err 
-ParamsSetup(
-    PF_InData		*in_data,
-    PF_OutData		*out_data,
-    PF_ParamDef		*params[],
-    PF_LayerDef		*output)
-{
-    PF_Err err = PF_Err_NONE;
-    PF_ParamDef def;
-
-    // Input layer - mandatory parameter
-    AEFX_CLR_STRUCT(def);
-    PF_ADD_LAYER("Input", PF_LayerDefault_NONE, PARAM_INPUT);
-
-    // Add curve parameters for RGB channels
-    AEFX_CLR_STRUCT(def);
-    PF_ADD_CURVE("RGB Curve", 0, 0, 255, 255, PARAM_RGB_CURVE);
-    
-    AEFX_CLR_STRUCT(def);
-    PF_ADD_CURVE("Red Curve", 0, 0, 255, 255, PARAM_RED_CURVE);
-    
-    AEFX_CLR_STRUCT(def);
-    PF_ADD_CURVE("Green Curve", 0, 0, 255, 255, PARAM_GREEN_CURVE);
-    
-    AEFX_CLR_STRUCT(def);
-    PF_ADD_CURVE("Blue Curve", 0, 0, 255, 255, PARAM_BLUE_CURVE);
-
-    out_data->num_params = NUM_PARAMS;
-
-    return err;
-}
-
+// Update the Render function
 static PF_Err 
 Render(
     PF_InData		*in_data,
@@ -66,38 +18,57 @@ Render(
     PF_LayerDef		*output)
 {
     PF_Err err = PF_Err_NONE;
+    PF_LayerDef *input_layer = &params[PARAM_INPUT]->u.ld;
     
-    // TODO: Implement curve adjustment rendering
-    // This will involve:
-    // 1. Reading curve values for each channel
-    // 2. Applying curve transformations to pixel values
-    // 3. Handling color space conversions if necessary
-
+    AEGP_SuiteHandler suites(in_data->pica_basicP);
+    
+    // For each pixel in the frame
+    for (int y = 0; y < output->height; y++) {
+        PF_Pixel* inputP = (PF_Pixel*)((char*)input_layer->data + 
+                                      y * input_layer->rowbytes);
+        PF_Pixel* outputP = (PF_Pixel*)((char*)output->data + 
+                                       y * output->rowbytes);
+        
+        for (int x = 0; x < output->width; x++) {
+            // Apply curves to each channel
+            float normalizedR = inputP[x].red / 255.0f;
+            float normalizedG = inputP[x].green / 255.0f;
+            float normalizedB = inputP[x].blue / 255.0f;
+            
+            // Apply RGB curve first
+            normalizedR = g_rgbCurve.evaluate(normalizedR);
+            normalizedG = g_rgbCurve.evaluate(normalizedG);
+            normalizedB = g_rgbCurve.evaluate(normalizedB);
+            
+            // Apply individual channel curves
+            normalizedR = g_redCurve.evaluate(normalizedR);
+            normalizedG = g_greenCurve.evaluate(normalizedG);
+            normalizedB = g_blueCurve.evaluate(normalizedB);
+            
+            // Convert back to 8-bit color
+            outputP[x].red = static_cast<A_u_char>(normalizedR * 255.0f);
+            outputP[x].green = static_cast<A_u_char>(normalizedG * 255.0f);
+            outputP[x].blue = static_cast<A_u_char>(normalizedB * 255.0f);
+            outputP[x].alpha = inputP[x].alpha;
+        }
+    }
+    
     return err;
 }
 
-extern "C" DllExport
-PF_Err PluginDataEntryFunction(
-    PF_PluginDataPtr inPtr,
-    PF_PluginDataCB inPluginDataCallBackPtr,
-    SPBasicSuite* inSPBasicSuitePtr,
-    const char* inHostName,
-    const char* inHostVersion)
+// Add a new function to handle curve UI events
+static PF_Err
+HandleEvent(
+    PF_InData		*in_data,
+    PF_OutData		*out_data,
+    PF_ParamDef		*params[],
+    PF_LayerDef		*output,
+    PF_EventExtra	*event_extra)
 {
-    PF_Err result = PF_Err_INVALID_CALLBACK;
-
-    result = PF_REGISTER_EFFECT(
-        inPtr,
-        inPluginDataCallBackPtr,
-        "Crazy Curves", // Name
-        "ADBE Crazy Curves", // Match Name
-        "Sample", // Category
-        AE_RESERVED_INFO
-    );
-
-    return result;
+    return g_curvesUI.handleEvent(in_data, out_data, params, output, event_extra);
 }
 
+// Update EffectMain to handle UI events
 PF_Err
 EffectMain(
     PF_Cmd			cmd,
@@ -112,17 +83,19 @@ EffectMain(
     try {
         switch (cmd) {
             case PF_Cmd_ABOUT:
-                // TODO: Implement About dialog
+                err = About(in_data, out_data, params, output);
                 break;
             case PF_Cmd_GLOBAL_SETUP:
-                out_data->my_version = PF_VERSION(MAJOR_VERSION, MINOR_VERSION, BUG_VERSION, STAGE_VERSION, BUILD_VERSION);
-                out_data->out_flags = PF_OutFlag_DEEP_COLOR_AWARE;
+                err = GlobalSetup(in_data, out_data, params, output);
                 break;
             case PF_Cmd_PARAMS_SETUP:
                 err = ParamsSetup(in_data, out_data, params, output);
                 break;
             case PF_Cmd_RENDER:
                 err = Render(in_data, out_data, params, output);
+                break;
+            case PF_Cmd_EVENT:
+                err = HandleEvent(in_data, out_data, params, output, (PF_EventExtra*)extra);
                 break;
             default:
                 break;
