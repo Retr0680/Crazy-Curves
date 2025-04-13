27481e55299d40a47fefd7a1f4a21cdf55f027b6
Retr0 <dedsecretr0680@gmail.com>
2025-04-13 18:17:06 +0530

    Files were in the wrong directory

---
+++ src/CurvesData.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+struct CurvePoint {
+    float x;
+    float y;
+    
+    CurvePoint(float x = 0.0f, float y = 0.0f) : x(x), y(y) {}
+    
+    bool operator<(const CurvePoint& other) const {
+        return x < other.x;
+    }
+};
+
+class CurveData {
+public:
+    CurveData();
+    void addPoint(float x, float y);
+    void removePoint(float x, float y, float tolerance = 0.01f);
+    float evaluate(float x) const;
+    void reset();
+    
+    const std::vector<CurvePoint>& getPoints() const { return points; }
+
+private:
+    std::vector<CurvePoint> points;
+    float interpolate(float x, const CurvePoint& p1, const CurvePoint& p2) const;
+    void sortPoints();
+};
