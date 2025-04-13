494197aa0f6bfd126bb38cd153eb9606293ffd9c
Retr0 <dedsecretr0680@gmail.com>
2025-04-13 18:05:14 +0530

    Update plugin.cpp

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -15,3 +15,80 @@ void Plugin::initialize() {
 void Plugin::registerPlugin() {
     // Code to register the plugin with After Effects
 }
+
+static PF_Err 
+ParamsSetup(
+    PF_InData* in_data,
+    PF_OutData* out_data,
+    PF_ParamDef* params[],
+    PF_LayerDef* output)
+{
+    PF_Err err = PF_Err_NONE;
+    PF_ParamDef def;
+
+    // Add curve control parameters
+    AEFX_CLR_STRUCT(def);
+    PF_ADD_FLOAT_SLIDER("Master Curve", -100, 100, 0, 100, 0, 0, 0, 0, 0, CRAZY_CURVES_MASTER_POINTS);
+    
+    AEFX_CLR_STRUCT(def);
+    PF_ADD_FLOAT_SLIDER("Red Curve", -100, 100, 0, 100, 0, 0, 0, 0, 0, CRAZY_CURVES_RED_POINTS);
+    
+    AEFX_CLR_STRUCT(def);
+    PF_ADD_FLOAT_SLIDER("Green Curve", -100, 100, 0, 100, 0, 0, 0, 0, 0, CRAZY_CURVES_GREEN_POINTS);
+    
+    AEFX_CLR_STRUCT(def);
+    PF_ADD_FLOAT_SLIDER("Blue Curve", -100, 100, 0, 100, 0, 0, 0, 0, 0, CRAZY_CURVES_BLUE_POINTS);
+
+    out_data->num_params = CRAZY_CURVES_NUM_PARAMS;
+    return err;
+}
+
+static PF_Err
+Render(
+    PF_InData* in_data,
+    PF_OutData* out_data,
+    PF_ParamDef* params[],
+    PF_LayerDef* output)
+{
+    PF_Err err = PF_Err_NONE;
+    AEGP_SuiteHandler suites(in_data->pica_basicP);
+    
+    // Get pixel data
+    PF_EffectWorld* input = &params[CRAZY_CURVES_INPUT]->u.ld;
+    PF_Pixel8* inputPixels = (PF_Pixel8*)input->data;
+    PF_Pixel8* outputPixels = (PF_Pixel8*)output->data;
+
+    // Apply curve adjustments for each channel
+    // TODO: Implement curve interpolation and color adjustment
+
+    return err;
+}
+
+PF_Err
+EffectMain(
+    PF_Cmd cmd,
+    PF_InData* in_data,
+    PF_OutData* out_data,
+    PF_ParamDef* params[],
+    PF_LayerDef* output,
+    void* extra)
+{
+    PF_Err err = PF_Err_NONE;
+
+    switch (cmd) {
+        case PF_Cmd_ABOUT:
+            err = About(in_data, out_data, params, output);
+            break;
+        case PF_Cmd_GLOBAL_SETUP:
+            out_data->my_version = PF_VERSION(1, 0, 0, 0, 0);
+            out_data->out_flags = PF_OutFlag_DEEP_COLOR_AWARE;
+            break;
+        case PF_Cmd_PARAMS_SETUP:
+            err = ParamsSetup(in_data, out_data, params, output);
+            break;
+        case PF_Cmd_RENDER:
+            err = Render(in_data, out_data, params, output);
+            break;
+    }
+    return err;
+}
